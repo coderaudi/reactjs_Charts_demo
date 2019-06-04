0f6069306b2d17210d7a224d8bcc560e801b260a
ABHIJEET KHIRE <abhijeetkhire@gmail.com>
2019-06-04 12:59:59 +0530

    doughtnutchart added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import "./styles.css";
 import LineChart from "./mycharts/linechart";
 import PieChart from "./mycharts/piechart";
 import BarChart from "./mycharts/barchart";
+import DoughnutChart from "./mycharts/doughnutchart";
 function App() {
   return (
     <div className="App">
       <LineChart />
       <PieChart />
       <BarChart />
+      <DoughnutChart />
     </div>
   );
 }
